51aa91673c8f1d453b78b8283daeafb228325192
Revolution-Roleplay-Servers <erlcrussiaroblox@gmail.com>
2026-06-07 19:44:08 +0300

    Фикс конфигов ботов

---
+++ Minzdrav/config.js
@@ -0,0 +1,7 @@
+const ICON_URL = "https://media.discordapp.net/attachments/1363556047704293517/1513215493090054395/bd4132725a515f1e.png?ex=6a26eb3f&is=6a2599bf&hm=a45b2d99329a554f02745c66669ef2c7d2ddf6e32f89b34b8395e9da1f7d86a5&=&format=webp&quality=lossless";
+const HEX_COLOR = 0xFFFFFF;
+
+module.exports = {
+  HEX_COLOR,
+  ICON_URL
+};
